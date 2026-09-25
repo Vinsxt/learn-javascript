2fcc5711e06a96268cff5c90961b5228c3016cc6
Vincent Hartono <vincentwhatwat175@gmail.com>
2026-09-25 14:39:31 +0700

    Implemented text visual that updates everytime a new todo-list is added, generating HTML <p> into <div> for the list

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,10 +1,22 @@
 const toDoList = [];
 
+function renderToDoList() {
+    let htmlList = '';
+    for (let i = 0; i < toDoList.length; i++) {
+        const task = toDoList[i];
+        const html = `<p>${task}</p>`;
+        htmlList += html;
+        console.log(htmlList);
+    }
+    document.querySelector('.list-div').innerHTML = htmlList;
+}
+
 function addToDo() {
     const inputElement = document.querySelector('.input-textbox');
     const task = inputElement.value;
     inputElement.value = ''; // reset the visuals after adding the task
-    
+
     toDoList.push(task);
     console.log(toDoList);
+    renderToDoList();
 }
